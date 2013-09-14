831b8880abf6fe377ec21baec51c995a06cfffcf
operationmech@gmail.com <operationmech@gmail.com>
2013-09-14 13:48:31 -0400

    changed spacing and fixed syntax also added the next case

--- HW1_Queries_1.sql
+++ HW1_Queries_1.sql
@@ -1,26 +1,27 @@
--- CAP2 Queries 1
+﻿-- CAP2 Queries 1
 -- Anthony Cali
 -- Sept 15, 2013
 
 --List all customers.
-
 SELECT *
 FROM customers;
 
 --List all agents named Smith.
-
 SELECT name, city
-WHERE name = "Smith"
-FROM agents;
+FROM agents
+WHERE name = 'Smith';
+
 
 --List products (pid, name, and quantity) over USD 1.50.
-
 SELECT pid, name, quantity
-WHERE priceusd > 1.50
-FROM products;
+FROM products
+WHERE priceusd > 1.50;
 
 --List the order number and agent id of all orders.
-
 SELECT ordno, aid
-WHERE *
 FROM orders;
+
+--List the names and cities of customers not in Dallas.
+SELECT name, city
+FROM customers
+WHERE city <> 'Dallas';
